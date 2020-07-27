4e762a63221bc5f016c7dc68a2baca69e2c6ea5f
Rifqi Fauzi <itdev.support20@tokiomarine-life.co.id>
2020-07-27 10:06:09 +0700

    generated model class

--- src/main/java/com/api/standalone/GenerateModel.java
+++ src/main/java/com/api/standalone/GenerateModel.java
@@ -1,7 +1,9 @@
 package com.api.standalone;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintStream;
+import java.io.UnsupportedEncodingException;
 import java.math.BigDecimal;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
@@ -28,34 +30,49 @@ import com.squareup.javapoet.TypeSpec;
 
 public class GenerateModel {
 
+	/*set model package*/
 	private static final String PACKAGE = "com.api.standalone.model";
 	
+	/*execute generate*/
 	public static void main(String[] args) throws SQLException, IOException {
-		generate("cities");
+		generate("persons");
 	}
 	
 	static void generate(String tableName) throws SQLException, IOException {
+		/*open connection*/ 
 		Base.open();
+		String lowerTableName = tableName.toLowerCase();
 		Statement stmt = Base.connection().createStatement();
+		
+		/*select data*/
         ResultSet rs = stmt.executeQuery("select * from ".concat(tableName));
         ResultSetMetaData rsmd = rs.getMetaData();
         List<MethodSpec> methodSpecs = new LinkedList<>();
         for (int i=1; i<=rsmd.getColumnCount(); i++) {
             String columnName = rsmd.getColumnName(i);
+            
+            /*Skip jika column ID karena di javalite sudah ada getId, bisa tidak diskip jika ID di database bukan auto increment*/
             if (columnName.equalsIgnoreCase("id"))
             	continue;
+            
+            /*get Class berdasarkan tipe column*/
             Class<?> columnClass = getColumnClass(rsmd.getColumnType(i));
+            
+            /*menentukan type method berdasarkan tipe column*/
             String typedMethodName = getColumnTypeMethodName(rsmd.getColumnType(i));
 
-            MethodSpec getMethod = MethodSpec.methodBuilder("get" + toTitleCase(columnName))
+            /*create getter method*/
+            MethodSpec getMethod = MethodSpec.methodBuilder("get" + toTitleCase(columnName.toLowerCase()))
                     .addModifiers(Modifier.PUBLIC)
                     .returns(columnClass)
                     .addStatement("return get$L($S)", typedMethodName, columnName)
                     .build();
             methodSpecs.add(getMethod);
 
-            String parameterName = toCamelCase(columnName);
-            MethodSpec setMethod = MethodSpec.methodBuilder("set" + toTitleCase(columnName))
+            String parameterName = toParamCase(columnName.toLowerCase());
+            
+            /*create setter method*/
+            MethodSpec setMethod = MethodSpec.methodBuilder("set" + toTitleCase(columnName.toLowerCase()))
                     .addModifiers(Modifier.PUBLIC)
                     .addParameter(columnClass, parameterName)
                     .returns(void.class)
@@ -64,23 +81,26 @@ public class GenerateModel {
             methodSpecs.add(setMethod);
         }
         
-        TypeSpec typeSpec = TypeSpec.classBuilder(toTitleCase(tableName))
-        		.addAnnotation(AnnotationSpec.builder(Table.class).addMember("value", "$S", tableName).build())
-        		.addAnnotation(AnnotationSpec.builder(IdName.class).addMember("value", "$S", "id").build())
-                .superclass(Model.class)
-                .addModifiers(Modifier.PUBLIC)
-                .addMethods(methodSpecs)
-                .build();
+        /*create Annotation berdasarkan nama table dan ID name saya hardcode dengan nama column "id" */
+        TypeSpec typeSpec = TypeSpec.classBuilder(toTitleCase(lowerTableName))
+					.addAnnotation(AnnotationSpec.builder(Table.class).addMember("value", "$S", tableName).build())
+					.addAnnotation(AnnotationSpec.builder(IdName.class).addMember("value", "$S", "id").build())
+					.superclass(Model.class).addModifiers(Modifier.PUBLIC).addMethods(methodSpecs).build();
+		writeJavaFile(lowerTableName, typeSpec);
 
-        JavaFile javaFile = JavaFile.builder(PACKAGE, typeSpec)
-                .build();
-
-        PrintStream stream = new PrintStream(fileName(tableName), "UTF-8");
-        javaFile.writeTo(stream);
-        
-        System.out.println(toTitleCase(tableName).concat(" generated"));
-        
+		/*close connection*/
 		Base.close();
+	}
+
+	/*write to Java file*/
+	private static void writeJavaFile(String lowerTableName, TypeSpec typeSpec)
+			throws FileNotFoundException, UnsupportedEncodingException, IOException {
+		JavaFile javaFile = JavaFile.builder(PACKAGE, typeSpec).build();
+
+		PrintStream stream = new PrintStream(fileName(lowerTableName), "UTF-8");
+		javaFile.writeTo(stream);
+
+		System.out.println(toTitleCase(lowerTableName).concat(" generated"));
 	}
 	
 	private static String fileName(String tableName) {
@@ -116,6 +136,7 @@ public class GenerateModel {
 	private static String getColumnTypeMethodName(int columnType) {
 		switch (columnType) {
 		case Types.VARCHAR:
+		case Types.NCHAR:
 			return "String";
 		case Types.DATE:
 			return "Date";
@@ -130,6 +151,8 @@ public class GenerateModel {
 		case Types.DECIMAL:
 		case Types.DOUBLE:
 			return "Double";
+		case Types.NUMERIC:
+			return "BigDecimal";
 		default:
 			return "";
 		}
@@ -148,6 +171,15 @@ public class GenerateModel {
 		return capitalize(camelCaseString.toString());
 	}
 
+	private static String toParamCase(String s) {
+		String[] parts = s.split("_");
+		StringBuilder paramString = new StringBuilder();
+		for (int i = 0; i < parts.length; i++) {
+			paramString.append(i==0?parts[i]:capitalize(parts[i]));
+		}
+		return paramString.toString();
+	}
+	
 	private static String capitalize(String original) {
 		if (original == null || original.length() == 0) {
 			return original;
